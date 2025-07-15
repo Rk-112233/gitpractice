5a3a5ed1e4219f43e39e635e6b1637e1bb3a0d3f
Rk-112233 <yervaranjith.reddy@gmail.com>
2025-07-15 16:23:17 +0530

    main

---
+++ test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	for {
+		fmt.Println("🚀 --------------MAIN--------------------🚀", time.Now())
+		fmt.Println("🚀 --------------MAIN--------------------🚀", time.Now())
+		fmt.Println("🚀 --------------MAIN--------------------🚀", time.Now())
+		fmt.Println("🚀 --------------MAIN--------------------🚀", time.Now())
+		fmt.Println("🚀 --------------MAIN--------------------🚀", time.Now())
+		fmt.Println("🚀 --------------MAIN--------------------🚀", time.Now())
+		fmt.Println("🚀 --------------MAIN--------------------🚀", time.Now())
+		time.Sleep(5 * time.Second)
+	}
+}
+ 
