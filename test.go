bf7b2e72e872909150f826051da5ef928b45d01d
Rk-112233 <yervaranjith.reddy@gmail.com>
2025-07-16 11:51:14 +0530

    testfile updated

--- test.go
+++ test.go
@@ -11,7 +11,7 @@ func main() {
 		fmt.Println("🚀 --------------copy1--------------------🚀", time.Now())
 		fmt.Println("🚀 --------------MAIN--------------------🚀", time.Now())
 		fmt.Println("🚀 --------------copy1--------------------🚀", time.Now())
-		fmt.Println("🚀 --------------MAIN--------------------🚀", time.Now())
+		fmt.Println("🚀 --------------MAIN1--------------------🚀", time.Now())
 		fmt.Println("🚀 --------------copy1--------------------🚀", time.Now())
 		fmt.Println("🚀 --------------copy2--------------------🚀", time.Now())
 		time.Sleep(5 * time.Second)
