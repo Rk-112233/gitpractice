321e42948decf3a2b10c1c3990d56e8ad2e3ca82
Rk-112233 <yervaranjith.reddy@gmail.com>
2025-07-16 14:49:36 +0530

    testfile updated

--- test.go
+++ test.go
@@ -10,11 +10,10 @@ func main() {
 		fmt.Println("🚀 --------------MAIN--------------------🚀", time.Now())
 		fmt.Println("🚀 --------------copy1--------------------🚀", time.Now())
 		fmt.Println("🚀 --------------ADVAITH--------------------🚀", time.Now())
-		fmt.Println("🚀 --------------copy1--------------------🚀", time.Now())
+		fmt.Println("🚀 --------------Ranjith--------------------🚀", time.Now())
 		fmt.Println("🚀 --------------MAIN1--------------------🚀", time.Now())
 		fmt.Println("🚀 --------------copy1--------------------🚀", time.Now())
 		fmt.Println("🚀 --------------copy2--------------------🚀", time.Now())
 		time.Sleep(5 * time.Second)
 	}
-}
  
