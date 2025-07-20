ec5da67f9ce0850a95714c31b23d69e52706483f
Rk-112233 <146756491+Rk-112233@users.noreply.github.com>
2025-07-20 14:10:57 +0530

    Update test.go

--- test.go
+++ test.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	for {
-		fmt.Println("🚀 --------------MAIN_changing@@...--------------------🚀", time.Now())
+		fmt.Println("🚀 --------------MAIN_changing@@ :)...--------------------🚀", time.Now())
 		fmt.Println("🚀 --------------Ram---------🚀", time.Now())
 		time.Sleep(5 * time.Second)
 	}
