b258450dcbcc9f547f9036d7fb7c04fc6febe595
Rk-112233 <146756491+Rk-112233@users.noreply.github.com>
2025-07-21 11:06:13 +0530

    Update test.go

--- test.go
+++ test.go
@@ -9,6 +9,7 @@ func main() {
 	// for {
 		fmt.Println("🚀 --------------%hello Ranjith%--------------------🚀", time.Now()) 
 		fmt.Println("🚀 --------------Rk---------🚀", time.Now())
+		fmt.Println("🚀 --------------RkR---------🚀", time.Now())
 		time.Sleep(5 * time.Second)
 	// }
  
